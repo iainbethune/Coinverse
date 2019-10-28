805a4270dc59ce1005873f6d0fac7fedc26eada8
AdamHurwitz <adam@coinverse.app>
2019-10-27 23:20:22 -0700

    Refactor - MainCoroutineExtension POC

--- app/src/test/java/app/coinverse/contentviewmodel/tests/FeedLoadContentTests.kt
+++ app/src/test/java/app/coinverse/contentviewmodel/tests/FeedLoadContentTests.kt
@@ -20,23 +20,24 @@ import app.coinverse.utils.models.ToolbarState
 import com.crashlytics.android.Crashlytics
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import io.mockk.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
 @ExtendWith(InstantExecutorExtension::class)
 class FeedLoadContentTests {
-    private val testDispatcher = TestCoroutineDispatcher()
+
+    companion object {
+        @JvmField
+        @RegisterExtension
+        val mainCoroutineExtension = MainCoroutineExtension()
+    }
+
     private val contentViewModel = ContentViewModel()
     private fun FeedLoad() = feedLoadTestCases()
 
@@ -54,63 +55,54 @@ class FeedLoadContentTests {
         unmockkAll() // Re-assigns transformation of object to original state prior to mock.
     }
 
-    @BeforeEach
-    fun beforeEach() {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @AfterEach
-    fun afterEach() {
-        Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
-    }
-
     @ParameterizedTest
     @MethodSource("FeedLoad")
-    fun `Feed Load`(test: FeedLoadContentTest) = testDispatcher.runBlockingTest {
-        mockComponents(test)
-        FeedLoad(test.feedType, test.timeframe, false).also { event ->
-            contentViewModel.processEvent(event)
-            assertThatToolbarState(test)
-            assertContentList(test, event)
-        }
-        verifyTests(test)
-    }
-
-    @ParameterizedTest
-    @MethodSource("FeedLoad")
-    fun `Swipe-to-Refresh`(test: FeedLoadContentTest) = testDispatcher.runBlockingTest {
-        mockComponents(test)
-        FeedLoad(test.feedType, test.timeframe, false).also { event ->
-            contentViewModel.processEvent(event)
-            // TODO - Remove
-            assertContentList(test, event)
-        }
-        SwipeToRefresh(test.feedType, test.timeframe, false).also { event ->
-            contentViewModel.processEvent(event)
-            assertContentList(test, event)
-            contentViewModel.feedViewState().contentList.getOrAwaitValue().also { pagedList ->
-                assertThat(pagedList).isEqualTo(test.mockFeedList)
-                println("FIX_TEXT Observe contentList ${event}")
-                if (test.feedType == MAIN)
-                    when (test.lceState) {
-                        LOADING -> contentViewModel.viewEffects()
-                                .swipeToRefresh.observe().also { effect ->
-                            assertThat(effect).isEqualTo(SwipeToRefreshEffect(true))
-                        }
-                        CONTENT -> contentViewModel.viewEffects()
-                                .swipeToRefresh.observe().also { effect ->
-                            assertThat(effect).isEqualTo(SwipeToRefreshEffect(false))
-                        }
-                        ERROR -> contentViewModel.viewEffects()
-                                .swipeToRefresh.observe().also { effect ->
-                            assertThat(effect).isEqualTo(SwipeToRefreshEffect(false))
-                        }
-                    }
+    fun `Feed Load`(test: FeedLoadContentTest) =
+            mainCoroutineExtension.testDispatcher.runBlockingTest {
+                mockComponents(test)
+                FeedLoad(test.feedType, test.timeframe, false).also { event ->
+                    contentViewModel.processEvent(event)
+                    assertThatToolbarState(test)
+                    assertContentList(test, event)
+                }
+                verifyTests(test)
             }
-        }
-        verifyTests(test)
-    }
+
+    @ParameterizedTest
+    @MethodSource("FeedLoad")
+    fun `Swipe-to-Refresh`(test: FeedLoadContentTest) =
+            mainCoroutineExtension.testDispatcher.runBlockingTest {
+                mockComponents(test)
+                FeedLoad(test.feedType, test.timeframe, false).also { event ->
+                    contentViewModel.processEvent(event)
+                    // TODO - Remove
+                    assertContentList(test, event)
+                }
+                SwipeToRefresh(test.feedType, test.timeframe, false).also { event ->
+                    contentViewModel.processEvent(event)
+                    assertContentList(test, event)
+                    contentViewModel.feedViewState().contentList.getOrAwaitValue().also { pagedList ->
+                        assertThat(pagedList).isEqualTo(test.mockFeedList)
+                        println("FIX_TEXT Observe contentList ${event}")
+                        if (test.feedType == MAIN)
+                            when (test.lceState) {
+                                LOADING -> contentViewModel.viewEffects()
+                                        .swipeToRefresh.observe().also { effect ->
+                                    assertThat(effect).isEqualTo(SwipeToRefreshEffect(true))
+                                }
+                                CONTENT -> contentViewModel.viewEffects()
+                                        .swipeToRefresh.observe().also { effect ->
+                                    assertThat(effect).isEqualTo(SwipeToRefreshEffect(false))
+                                }
+                                ERROR -> contentViewModel.viewEffects()
+                                        .swipeToRefresh.observe().also { effect ->
+                                    assertThat(effect).isEqualTo(SwipeToRefreshEffect(false))
+                                }
+                            }
+                    }
+                }
+                verifyTests(test)
+            }
 
     private fun mockComponents(test: FeedLoadContentTest) {
 
---
+++ app/src/test/java/app/coinverse/utils/MainCoroutineExtension.kt
@@ -0,0 +1,22 @@
+package app.coinverse.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+class MainCoroutineExtension : BeforeEachCallback, AfterEachCallback {
+    val testDispatcher = TestCoroutineDispatcher()
+
+    override fun beforeEach(context: ExtensionContext?) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+}
